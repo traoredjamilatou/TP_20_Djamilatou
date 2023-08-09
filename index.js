326bb02e230c94702d003c8e5b9db095e369118d
djami <djamilatraore31@gmail.com>
2023-08-09 17:03:54 +0000

    TP-20

---
+++ index.js
@@ -0,0 +1,118 @@
+const nos = document.querySelector(".nos");
+const B = document.querySelector(".B");
+const D = document.querySelector(".D");
+const C = document.querySelector(".C");
+nos.addEventListener("click", () => {
+    B.style.display = "none";
+    D.style.display = "flex";
+    C.style.display = "none";
+});
+const not = document.querySelector(".not");
+not.addEventListener("click", () => {
+    B.style.display = "flex";
+    D.style.display = "none";
+    C.style.display = "none";
+});
+const nom = document.querySelector(".nom");
+nom.addEventListener("click", () => {
+    C.style.display = "flex";
+    B.style.display = "none";
+    D.style.display = "none";
+});
+
+const mo = document.querySelector(".mo");
+mo.addEventListener("click", ()=> {
+ B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";  
+});
+const mot = document.querySelector(".mot");
+mot.addEventListener("click", ()=> {
+ B.style.display = "flex";
+ D.style.display = "none";
+ C.style.display = "none";  
+});
+const mob = document.querySelector(".mob");
+mob.addEventListener("click", ()=> {
+ B.style.display = "none";
+ D.style.display = "none";
+ C.style.display = "flex";  
+});
+
+const U = document.querySelector(".U");
+U.addEventListener("click", ()=> {
+ B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";  
+});
+const UR = document.querySelector(".UR");
+UR.addEventListener("click", ()=> {
+ B.style.display = "flex";
+ D.style.display = "none";
+ C.style.display = "none";  
+});
+const URM = document.querySelector(".URM");
+URM.addEventListener("click", ()=> {
+ B.style.display = "flex";
+ D.style.display = "none";
+ C.style.display = "none";  
+});
+
+const violet = document.querySelector(".violet");
+B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";
+violet.addEventListener("click", () => {
+    B.style.display = "flex";
+    D.style.display = "none";
+    C.style.display = "none";
+});
+violet.addEventListener("click", () => {
+    C.style.display = "none";
+    B.style.display = "flex";
+    D.style.display = "none";
+});
+const DJ = document.querySelector(".DJ");
+B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";
+ DJ.addEventListener("click", () => {
+    B.style.display = "flex";
+    D.style.display = "none";
+    C.style.display = "none";
+});
+DJ.addEventListener("click", () => {
+    C.style.display = "none";
+    B.style.display = "none";
+    D.style.display = "flex";
+});
+const rose = document.querySelector(".rose");
+B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";
+ rose.addEventListener("click", () => {
+    B.style.display = "flex";
+    D.style.display = "none";
+    C.style.display = "none";
+});
+rose.addEventListener("click", () => {
+    C.style.display = "flex";
+    B.style.display = "none";
+    D.style.display = "none";
+});
+const v = document.querySelector(".v");
+B.style.display = "none";
+ D.style.display = "flex";
+ C.style.display = "none";
+ v.addEventListener("click", () => {
+    B.style.display = "flex";
+    D.style.display = "none";
+    C.style.display = "none";
+});
+v.addEventListener("click", () => {
+    C.style.display = "none";
+    B.style.display = "flex";
+    D.style.display = "none";
+});
+
+
